adebbcab8d9621ad87bf7360cb72b973cb84521e
EyeMLeo <Leonid.Akuta@gmail.com>
2022-11-15 16:27:40 +0100

    6 uzd ALL DONE js only

--- Task 6/script.js
+++ Task 6/script.js
@@ -9,13 +9,32 @@ atveju grąžins visų "users" vardus naujame masyve pvz., ['John Smith', 'Ann S
 -------------------------------------------------------------------------- */
 
 const users = [
-  { id: '1', name: 'John Smith', age: 20 },
-  { id: '2', name: 'Ann Smith', age: 24 },
-  { id: '3', name: 'Tom Jones', age: 31 },
-  { id: '4', name: 'Rose Peterson', age: 17 },
-  { id: '5', name: 'Alex John', age: 25 },
-  { id: '6', name: 'Ronald Jones', age: 63 },
-  { id: '7', name: 'Elton Smith', age: 16 },
-  { id: '8', name: 'Simon Peterson', age: 30 },
-  { id: '9', name: 'Daniel Cane', age: 51 },
+  { id: "1", name: "John Smith", age: 20 },
+  { id: "2", name: "Ann Smith", age: 24 },
+  { id: "3", name: "Tom Jones", age: 31 },
+  { id: "4", name: "Rose Peterson", age: 17 },
+  { id: "5", name: "Alex John", age: 25 },
+  { id: "6", name: "Ronald Jones", age: 63 },
+  { id: "7", name: "Elton Smith", age: 16 },
+  { id: "8", name: "Simon Peterson", age: 30 },
+  { id: "9", name: "Daniel Cane", age: 51 },
 ];
+
+// average age counting
+function getUserAverageAge(arr) {
+  return (
+    arr.reduce((acc, element) => (acc += element.age), 0) /
+    (arr.length + 1)
+  ).toFixed(2);
+}
+
+console.log("getUserAverageAge(users) ===", getUserAverageAge(users));
+
+// new array
+function newNameArray(arr) {
+  return arr.reduce((acc, element) => {
+    acc.push(element.name);
+    return acc;
+  }, []);
+}
+console.log("newNameArray(users) ===", newNameArray(users));
