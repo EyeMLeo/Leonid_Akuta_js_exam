f7e749152693d65dfb97dc53e60364eb135f554f
EyeMLeo <Leonid.Akuta@gmail.com>
2022-11-15 17:01:26 +0100

    8 uzd ALL DONE JS only

--- Task 8/script.js
+++ Task 8/script.js
@@ -5,3 +5,26 @@ subtraction(a, b) - priima du skaičius ir grąžina jų skirtumą.
 multiplication(a, b) - priima du skaičius ir grąžina jų daugybos rezultatą;
 division(a, b) - priima du skaičius ir grąžina jų dalybos rezultatą;
 ------------------------------------------------------------------------------------ */
+
+class Calculator {
+  constructor(a, b) {
+    this.sum = this.sum(a, b);
+    this.subtraction = this.subtraction(a, b);
+    this.multiplication = this.multiplication(a, b);
+    this.division = this.division(a, b);
+  }
+  sum(a, b) {
+    return +a + b;
+  }
+  subtraction(a, b) {
+    return a - b;
+  }
+  multiplication(a, b) {
+    return a - b;
+  }
+  division(a, b) {
+    return a / b;
+  }
+}
+let resultCalculate = new Calculator(16, 4);
+console.log("resultCalculate ===", resultCalculate);
