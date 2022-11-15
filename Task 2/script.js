c28eac95812a75cd2386eb6ba9fb940c80034ca4
EyeMLeo <Leonid.Akuta@gmail.com>
2022-11-15 08:50:28 +0100

    uzd2 planas

--- Task 2/script.js
+++ Task 2/script.js
@@ -3,3 +3,13 @@ Parašykite JS kodą, kuris skaičiuos kiek kartų buvo paspaustas mygtukas
 su tekstu "CLICK ME". Paspaudimų rezultatas turi būti matomas dešinėje
 pusėje esančiame "state" skaičiavimo bloke (<div id="btn__state">0</div>)
 ------------------------------------------------------------------- */
+
+// 2 contantos ir 1 kintamasis
+// - paspaudimo mygtukas
+// -  Score atvaizdavimas
+// -  Score rezultatas
+
+// mygtuko nuskaitymo funkcija
+// - nuskaitom score atvaizduota reiksme
+// - prie nuskaitytos reiksmes pridedam 1
+// - atvaizduojam nauja score reiksme
